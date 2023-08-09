653f19049a6bcebb96e9d7211341d103bff86e85
virtus <AleksySi2509@protonmail.com>
2023-08-09 04:34:24 +0100

    Added basic token types

--- Lexer/token.go
+++ Lexer/token.go
@@ -1,1 +1,29 @@
 package lexer
+
+type TokenType int
+
+const (
+	// End of file
+	EOF TokenType = iota
+
+	// Identifier & Literal
+	Identifier
+	Number
+
+	//Operators
+	Add
+	Minus
+	Assign
+
+	//Delimiters
+	EndOfLine
+
+	//Keywords
+	Function
+	Let
+)
+
+type Token struct {
+	Type  TokenType
+	Value string
+}
