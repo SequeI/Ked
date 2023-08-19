7d14f9b90821ad9c0fcf6b8757b0f9441e896919
virtus <AleksySi2509@protonmail.com>
2023-08-20 00:50:53 +0100

    Restart

--- Lexer/lexer.go
+++ Lexer/lexer.go
@@ -1,9 +1,5 @@
 package lexer
 
-// import "unicode"
-
-// Inserting source code into a struct
-
 type Lexer struct {
 	input  string
 	pos    int
@@ -20,34 +16,9 @@ func NewLexer(input string) *Lexer {
 	}
 }
 
-// Methods for tokenizing source code
-
-func (l *Lexer) nextChar() {
-	l.pos++
-	if l.pos < l.length {
-		l.char = l.input[l.pos]
-	} else {
-		l.char = byte(EOF)
-	}
-}
-
-func isWhitespace(char byte) bool {
-	// FUTURE COLUMN + ROW TRACKER FOR ERROR ACCURACY
-	return char == ' ' || char == '\t' || char == '\n' || char == '\r'
-}
-
 // tokenizer function
 func Tokenizer(input string) string {
 	lexer := NewLexer(input)
-
-	for lexer.char != byte(EOF) {
-		lexer.nextChar()
-		//lint:ignore SA4017 Ignore this warning because the isWhitespace function is used for code organization.
-		if isWhitespace(lexer.char) {
-			// FUTURE COLUMN + ROW TRACKER FOR ERROR ACCURACY
-			continue
-		}
-	}
 
 	return " "
 }
