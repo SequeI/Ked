43022e052500b0bf6346adc3356af7952d985f9e
virtus <AleksySi2509@protonmail.com>
2023-08-22 22:34:15 +0100

    Restarting lexer, actually thought it through now

--- Lexer/lexer.go
+++ Lexer/lexer.go
@@ -1,86 +1,19 @@
 package lexer
 
-import (
-	"fmt"
-	"unicode"
-)
-
-type Lexer struct {
-	input  string
-	pos    int
-	length int
-}
-
-func NewLexer(input string) *Lexer {
-	return &Lexer{
-		input:  input,
-		pos:    0,
-		length: len(input),
-	}
-}
-
-// Functions + Methods
-func isWhitespace(char rune) bool {
-	return unicode.IsSpace(char)
-}
-
-func isAlphabetChar(s string) bool {
-	for _, char := range s {
-		if unicode.IsLetter(char) {
-			return true
-		}
-	}
-	return false
-}
-
 // tokenizer function
 func Tokenizer(input string) []Token {
 
+	input += "\n"
+	current := 0
 	tokens := []Token{}
-	lexer := NewLexer(input)
 
-	for lexer.pos < lexer.length {
-		char := lexer.input[lexer.pos]
+	for current < len([]rune(input)) {
 
-		if isWhitespace(rune(char)) {
-			lexer.pos++
-			continue
-		}
-		tempV := string(char)
+		char := string([]rune(input)[current])
 
-		for lexer.pos+1 < lexer.length {
-			nextChar := lexer.input[lexer.pos+1]
-			if isWhitespace(rune(nextChar)) {
-				break
-			}
-			tempV += string(nextChar)
-			lexer.pos++
-		}
-		if _, ok := keywordTokens[tempV]; ok {
-			token := Token{Type: keywordTokens[tempV], Value: tempV}
-			tokens = append(tokens, token)
-		} else {
-			if isAlphabetChar(tempV) {
-				token := Token{Type: IDENTIFIER, Value: tempV}
-				tokens = append(tokens, token)
-			} else {
-				// Probably integer if doesn't contain letters lol, will do better checks later
-				token := Token{Type: INT, Value: tempV}
-				tokens = append(tokens, token)
-			}
+		if _, ok := keywordTokens[char]; ok && string([]rune(input)[current+1]) == " " {
+			tokens = append(tokens, Token{Type: keywordTokens[char], Value: char})
 		}
 	}
-
-	// Add EOF token at the end
-	token := Token{Type: EOF, Value: "EndOfFile"}
-	tokens = append(tokens, token)
-
 	return tokens
-}
-
-// For use when testing in lexer_test.go
-func PrintTokens(tokens []Token) {
-	for _, token := range tokens {
-		fmt.Printf("Type: %v, Value: %v\n", token.Type, token.Value)
-	}
 }
